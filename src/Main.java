200f3d4ed354ff9d51247c0cd4ce055612ebb926
Johnny Battah <johnny.battah@elev.ga.ntig.se>
2025-09-14 20:25:04 +0200

    First comit and push

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,51 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.Random;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    static Random randomGenerator = new Random();
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to my fighting game");
+
+
+        // Contains the stats of the first character (HP, Mana, Damage)
+        ArrayList<Integer> stats1 = new ArrayList<>();
+        stats1.add(100);
+        stats1.add(randomGenerator.nextInt(20, 81));
+        stats1.add(randomGenerator.nextInt(20, 81));
+
+        // Contains the stats of the first character (HP, Mana, Damage)
+        ArrayList<Integer> stats2 = new ArrayList<>();
+        stats2.add(100);
+        stats2.add(randomGenerator.nextInt(20, 81));
+        stats2.add(randomGenerator.nextInt(20, 81));
+
+        // Print character info to check it works
+        System.out.println("Character 1 has "+ stats1.get(1) + " mana");
+
+        int newHP = standardAttack(stats1.get(2), stats2.get(0));
+        stats2.set(0, newHP); // Puts the new HP to current HP in stats2
+        System.out.println("Fighter number 2 now has " + stats2.get(0) + " HP");
+
     }
+
+    // Method for standard attack
+    public static int standardAttack(int damage, int hp){
+        int chance = randomGenerator.nextInt(100); // Generates a numer 0-99
+
+        // Chance of crit is appx 1/3
+        if (chance<67) {
+            hp = hp - damage / 6;
+            System.out.println("Regular hit");
+        }else {
+            hp = hp-damage;
+            System.out.println("Critical hit");
+        }
+        return hp;
+    }
+
 }
+
+// System.out.println("Första"); // Skriver ut och skapar ny rad
+// System.out.print("Andra"); // Skriver ut utan att avsluta med ny rad
+// System.out.print("Tredje");
